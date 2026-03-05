a984e05799fd70668d1eb8c448ed71970683fc30
dragonborn <dragonborn@gmail.com>
2026-03-06 00:14:45 +0300

    make full main app

--- main/models.py
+++ main/models.py
@@ -1,32 +1,36 @@
 from django.db import models
 from django.utils.text import slugify
-# Create your models here.
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
     slug = models.CharField(max_length=100, unique=True)
+
 
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.name)
         super().save(*args, **kwargs)
 
+    
     def __str__(self):
-        return self.slug
+        return self.name
     
 
 class Size(models.Model):
-    name = models.CharField(max_length=25)
+    name = models.CharField(max_length=20)
+
 
     def __str__(self):
         return self.name
     
 
 class ProductSize(models.Model):
-    product = models.ForeignKey("Product", on_delete=models.CASCADE,
-                                related_name="product_size")
+    product = models.ForeignKey('Product', on_delete=models.CASCADE,
+                                related_name='product_sizes')
     size = models.ForeignKey(Size, on_delete=models.CASCADE)
-    stock = models.PositiveBigIntegerField(default=0)
+    stock = models.PositiveIntegerField(default=0)
+
 
     def __str__(self):
         return f"{self.size.name} ({self.stock} in stock) for {self.product.name}"
@@ -41,20 +45,22 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(blank=True)
     main_image = models.ImageField(upload_to='products/main/')
-    created_at = models.DateField(auto_now_add=True)
-    update_at = models.DateField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(self.name)
+            clean_name = self.name.replace("'", "")
+            self.slug = slugify(clean_name)
         super().save(*args, **kwargs)
 
+    
     def __str__(self):
-        return self.slug
-
+        return self.name
+    
 
 class ProductImage(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE,
-                                related_name="images")
-    image = models.ImageField(upload_to="product/extra/")
-    
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, 
+                                related_name='images')
+    image = models.ImageField(upload_to='products/extra/')
--- main/views.py
+++ main/views.py
@@ -7,37 +7,40 @@ from django.db.models import Q
 
 
 class IndexView(TemplateView):
-    template_name = "main/base.html"
+    template_name = 'main/base.html'
+
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["categories"] = Category.objects.all()
-        context["current_categories"] = None
+        context['categories'] = Category.objects.all()
+        context['current_category'] = None
         return context
     
+
     def get(self, request, *args, **kwargs):
         context = self.get_context_data(**kwargs)
-        if request.headers.get("HX-Request"):
-            return TemplateResponse(request, "main/home_content.html", context)
-        return TemplateResponse(request.self.template_name, context)
+        if request.headers.get('HX-Request'):
+            return TemplateResponse(request, 'main/home_content.html', context)
+        return TemplateResponse(request, self.template_name, context)
     
-    
+
 class CatalogView(TemplateView):
-    template = "main/base.html"
+    template = 'main/base.html'
 
     FILTER_MAPPING = {
-        "color": lambda queryset, value: queryset.filter(color__iexact=value),
-        "min_price": lambda queryset, value: queryset.filter(price_gte=value),
-        "max_price": lambda queryset, value: queryset.filter(price_lte=value),
-        "size": lambda queryset, value: queryset.filter(product_size__size__name=value),
+        'color': lambda queryset, value: queryset.filter(color__iexact=value),
+        'min_price': lambda queryset, value: queryset.filter(price__gte=value),
+        'max_price': lambda queryset, value: queryset.filter(price__lte=value),
+        'size': lambda queryset, value: queryset.filter(product_sizes__size__name=value),
     }
+
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        category_slug = kwargs.get("category_slug")
+        category_slug = kwargs.get('category_slug')
         categories = Category.objects.all()
-        products = Product.objects.all().order_by("created_at")
-        current_categoty = None
+        products = Product.objects.all().order_by('-created_at')
+        current_category = None
 
         if category_slug:
             current_category = get_object_or_404(Category, slug=category_slug)
@@ -46,7 +49,7 @@ class CatalogView(TemplateView):
         query = self.request.GET.get('q')
         if query:
             products = products.filter(
-                Q(name__icontains=query) and Q(description_icontains=query)
+                Q(name__icontains=query) | Q(description__icontains=query)
             )
 
         filter_params = {}
@@ -59,54 +62,57 @@ class CatalogView(TemplateView):
                 filter_params[param] = ''
 
         filter_params['q'] = query or ''
+
         context.update({
-            "categories": categories,
-            "products": products,
-            "current_category": category_slug,
-            "filter_params": filter_params,
-            "sizes": Size.objects.all(),
-            "search_query": query or "" 
+            'categories': categories,
+            'products': products,
+            'current_category': category_slug,
+            'filter_params': filter_params,
+            'sizes': Size.objects.all(),
+            'search_query': query or ''
         })
 
-        if self.request.GET.get("show_search") == "true":
-            context["show_search"] = True 
-        elif self.request.GET.get("reset_search") == "true":
-            context["reset_search"] == True
-
+        if self.request.GET.get('show_search') == 'true':
+            context['show_search'] = True
+        elif self.request.GET.get('reset_search') == 'true':
+            context['reset_search'] = True
+        
         return context
+    
 
     def get(self, request, *args, **kwargs):
         context = self.get_context_data(**kwargs)
-        if request.headers.get("HX-Request"):
-            if context.get("show_search "):
-                return TemplateResponse(request, 'main/search', context)
-            elif context.get('rest_search'):
-                return TemplateResponse(request, 'main/search_buttom.html')
-            template = 'main/filter_modal.html' if request.GET.get('show_filters') == 'true' else 'main/catalog.html'
-            return TemplateResponse(request, template, context)
+        if request.headers.get('HX-Request'):
+            if context.get('show_search'):
+                return TemplateResponse(request, 'main/search_input.html', context)
+            elif context.get('reset_search'):
+                return TemplateResponse(request, 'main/search_button.html', {})
+            template_name = 'main/filter_modal.html' if request.GET.get('show_filters') == 'true' else 'main/catalog.html'
+            return TemplateResponse(request, template_name, context)
         return TemplateResponse(request, self.template_name, context)
     
+
 class ProductDetailView(DetailView):
     model = Product
-    template_name = "main/base.html"
-    skug_field = "slug"
-    slug_url_kwarg = "slug"
+    template_name = 'main/base.html'
+    slug_field = 'slug'
+    slug_url_kwarg = 'slug'
+
 
     def get_context_data(self, **kwargs):
-        context = super().get_contextdata(**kwargs)
+        context = super().get_context_data(**kwargs)
         product = self.get_object()
-        context["categories"] = Category.objects.all()
-        context["related_products"] = Product.object.filter(
+        context['categories'] = Category.objects.all()
+        context['related_products'] = Product.objects.filter(
             category=product.category
         ).exclude(id=product.id)[:4]
-        context["cerrent_category"] = product.category.slug
-        return context            
+        context['current_category'] = product.category.slug
+        return context
+    
 
     def get(self, request, *args, **kwargs):
         self.object = self.get_object()
         context = self.get_context_data(**kwargs)
-        if request.header.get("HX-Reaquest"):
-            return TemplateResponse(request, "main/product/detail.html", context)
-        raise TemplateResponse(request, self.template_name, context)
-    
-
+        if request.headers.get('HX-Request'):
+            return TemplateResponse(request, 'main/product_detail.html', context)
+        return TemplateResponse(request, self.template_name, context)
